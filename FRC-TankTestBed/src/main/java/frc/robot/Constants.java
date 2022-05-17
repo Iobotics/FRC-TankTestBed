d6a47e52aac20263c211331e3260a4d1a06bbb84
Spryzen <bkt2401@iolani.org>
2022-05-16 15:53:21 -1000

    update turret

--- FRC-TankTestBed/src/main/java/frc/robot/Constants.java
+++ FRC-TankTestBed/src/main/java/frc/robot/Constants.java
@@ -18,13 +18,43 @@ public final class Constants {
         // Drivetrain devices (motors) 
         public static final int kLeftMaster = 1;
         public static final int kLeftSlave = 2;
-        public static final int kRightMaster = 3;
-        public static final int kRightSlave = 4;
+        public static final int kRightMaster = 4;
+        public static final int kRightSlave = 3;
     }
-    
+
     public static final class OIConstants{
         public static final int kJoystick1 = 1;
         public static final int kJoystick2 = 0;
         public static final int kXbox1 = 2;
+    }
+
+    public static final class TurretConstants{
+        public static final int kSpinner = 5;
+
+         //for the TalonSRX, a Error * kP of 1023 is considered "full". For our poteniometer, the typical range of motion is ~300-500, meaning
+        //the greatest error we will likely get is of 200, with the error more likely to be closer to 100 or 50 in a given circumstance.
+        //More info here: https://docs.ctre-phoenix.com/en/stable/ch16_ClosedLoop.html#position-closed-loop-control-mode
+        //good recomendatation for smooth control: Start "D" term ~10x to 100x P term to smoothen movement, and if not reacing target, Integral term should start .01x P gain
+        //in this case, 50  * our error of ~ 200  at a given point is going to be 400, wwhich is only about 1/2 full power, but is still enough. If our netural
+        //deadband is .04 of full power, which coriponds to an error of aproximately 40 encoder units, or 16 degrees. From experience, the arm begins
+        //to oscilate at ~100 units, which would mean an error of aproximately 8 degrees at the current neautral deadband.
+        /* 	                                    			  kP   kI   kD   kF   Iz  PeakOut */
+        public static final Gains kTurretGains = new Gains( 30.0, 0.0,  30.0, 0.0, 0,  1 ); //ARM PID values
+        public static final double kTicksPerDegree = (1023.0/10.0) * (170.0 /30.0) * (30.0/20.0) * (1./360.); 
+        public static final int kMeasuredPosHorizontal = -260;
+        public static final double kMaxGravityFF = .12;
+        //Target speed is in Degrees/second
+        public static final double kArmTargetSpeed = 40;
+
+
+    }
+
+    public static final class PIDConstants{
+        public static final int kPIDprimary = 0;
+        public static final int kPIDturn = 1;
+        public static final int kRemoteFilter0 = 0;
+        public static final int kRemoteFilter1 = 1;
+        public final static int kSlot0 = 0;
+        public final static int kSlot1 = 1;
     }
 }
--- FRC-TankTestBed/src/main/java/frc/robot/RobotContainer.java
+++ FRC-TankTestBed/src/main/java/frc/robot/RobotContainer.java
@@ -4,31 +4,18 @@
 
 package frc.robot;
 
-import java.sql.Time;
-
-import com.kauailabs.navx.frc.AHRS;
-
-import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.command.WaitCommand;
-import edu.wpi.first.wpilibj.motorcontrol.Spark;
-// import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Constants.OIConstants;
 import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Turret;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-import frc.robot.Constants.OIConstants;
-import edu.wpi.first.wpilibj.PWM;
-import edu.wpi.first.wpilibj.Servo;
+
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -40,14 +27,18 @@ public class RobotContainer {
   // The robot's subsystems and commands are defined here...
 
   private final Drivetrain drivetrain = new Drivetrain();
+  private final Turret turret = new Turret();
   private final Joystick joystick1 = new Joystick(OIConstants.kJoystick1);
   private final Joystick joystick2 = new Joystick(OIConstants.kJoystick2);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
+    
+    SmartDashboard.putNumber("turretPos:",0);
+    SmartDashboard.putNumber("Art Target Position:",0);
 
     drivetrain.setDefaultCommand(new RunCommand(
-      () -> drivetrain.setArcade(joystick1.getY(), -joystick2.getX()),drivetrain)
+      () -> drivetrain.setArcade(-joystick1.getY(), joystick2.getX()),drivetrain)
     );
     
   // Configure the button bindings
@@ -60,7 +51,17 @@ public class RobotContainer {
    * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
    * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
-  private void configureButtonBindings() {}
+  private void configureButtonBindings() {
+    // new JoystickButton(joystick1, 3).whenPressed(
+    //   new RunCommand(
+    //     ()-> turret.setSpinnerPower(joystick1.getZ()), turret)
+    // );
+
+    new JoystickButton(joystick1, 2).whenPressed(
+      new InstantCommand(
+        ()-> SmartDashboard.putNumber("turretPos", turret.getSpinnerPosition()))
+      );
+  }
 
   /**
    * Use this to pass the autonomous command to the main {@link Robot} class.
